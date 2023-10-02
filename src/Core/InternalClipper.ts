530c987cf78c880e83827897e48aff52fcd0b03d
uma-neko <36249844+uma-neko@users.noreply.github.com>
2023-10-02 14:53:08 +0900

    Followed rounding rules of the original code.

--- src/Core/InternalClipper.ts
+++ src/Core/InternalClipper.ts
@@ -2,7 +2,7 @@ import type { IPath64 } from "./IPath64";
 import { Point64 } from "./Point64";
 import { PointD } from "./PointD";
 import { PointInPolygonResult } from "../Engine/EngineEnums";
-import { numberToBigInt } from "../Clipper";
+import { numberToBigInt, roundToEven } from "../Clipper";
 
 const floatingPointTolerance = 1e-12;
 
@@ -112,8 +112,8 @@ export const getClosestPtOnSegment = (
     return Point64.clone(seg2);
   }
   return {
-    x: seg1.x + numberToBigInt(q * Number(dx)),
-    y: seg1.y + numberToBigInt(q * Number(dy)),
+    x: seg1.x + BigInt(roundToEven(q * Number(dx))),
+    y: seg1.y + BigInt(roundToEven(q * Number(dy))),
   };
 };
 
